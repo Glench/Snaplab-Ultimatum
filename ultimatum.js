e5bda7f9b6781b1331d83345d0e36477c8cf9751
Glen Chiacchieri <www@glench.com>
2012-05-28 13:24:24 -0400

    First pass at ultimatum models

---
+++ ultimatum.js
@@ -0,0 +1,84 @@
+var Ultimatum = function(numPlayers, totalAmount) {
+    // the game encapsulated so as not to pollute global namespace
+
+    var ultimatum = {
+            totalAmount: totalAmount, // total amount to be divvied up
+            players: [] // instances of the Player model
+        },
+        Player = function (params) {
+            // each player in the game
+            var playerDefaults = {
+                    id: null,
+                    isGiver: false, // is the one making the ultimatum
+                    isReceiver: true, // is one of the acceptor/rejectors
+                    amount: null, // the amount his player receives
+                    acceptedOffer: null // has accepted the offer or not
+                },
+                player = $.extend(player_defaults, params);
+
+                player.validate = function() {
+                    // make sure they have an id
+                    if (!$.isNumeric(player.id)) {
+                        return false;
+                    }
+
+                    // make sure they aren't a giver and receiver by using
+                    // not of xor
+                    if (!(!player.isGiver !== !player.isReceiver)) {
+                        return false;
+                    }
+
+                    // make sure they put in a number
+                    if (!$.isNumeric(player.amount)) {
+                        return false;
+                    }
+
+                    // make sure the amount is positive
+                    if (player.amount < 0) {
+                        return false;
+                    }
+
+                    return true;
+                };
+            return player;
+        };
+
+    // init functions
+    for (var i = 0; i < numPlayers; ++i) {
+        ultimatum.players[i] = Player({
+            id: i,
+            isGiver: i == 0,
+            isReceiver: i != 0
+        });
+    }
+
+    ultimatum.numPlayers = function() { return ultimatum.players.length; };
+
+    ultimatum.calculatedTotal = function() {
+        var total = 0, i;
+        for (i = 0; i < ultimatum.numPlayers(); ++i) {
+            if (ultimatum.players[i].amount) {
+                total += ultimatum.players[i].amount;
+            }
+        }
+    };
+
+    ultimatum.validate = function() {
+        // have to validate they didn't put an amount over the total that they
+        // were allowed to put
+        if (ultimatum.calculatedTotal() > ultimatum.totalAmount) {
+            return false;
+        }
+
+        // validate each player
+        for (var i = 0; i < ultimatum.numPlayers(); ++i) {
+            if (!ultimatum.players[i].validate()) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    return ultimatum;
+};
